4916ca92231a3e2a250138f059713c4d99670b0b
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-07-01 16:06:08 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/dev/checku/checkuscheduler/CheckuSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.checku.checkuscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckuSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckuSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/checku/checkuscheduler/CheckuSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.checku.checkuscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckuSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
