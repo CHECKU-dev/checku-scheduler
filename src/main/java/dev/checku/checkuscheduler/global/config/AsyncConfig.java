b8a99284fe9a15e0aa373687ca67e0037132defb
yoon-youngjin <dudwls143@gmail.com>
2023-05-25 23:30:51 +0900

    update

--- src/main/java/dev/checku/checkuscheduler/global/config/AsyncConfig.java
+++ src/main/java/dev/checku/checkuscheduler/global/config/AsyncConfig.java
@@ -3,7 +3,23 @@ package dev.checku.checkuscheduler.global.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.concurrent.Executor;
+
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+public class AsyncConfig extends AsyncConfigurerSupport {
+
+    private final int POOL_SIZE = 3;
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(POOL_SIZE);
+        executor.setMaxPoolSize(POOL_SIZE);
+        executor.setQueueCapacity(10);
+        executor.initialize();
+        return executor;
+    }
+
+
 }
